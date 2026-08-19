4a8ad1ebff2a1d3beeea4931189105a9b9655451
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-08-20 00:38:03 +0530

    Jai Maa Bhavani

--- src/Lec1/Lec.java
+++ src/Lec1/Lec.java
@@ -2,6 +2,6 @@ package Lec1;
 
 public class Lec {
     public static void main(String [] args) {
-        System.out.println("Hello World");
+        System.out.println("Jai Maa Bhagwati");
     }
 }
