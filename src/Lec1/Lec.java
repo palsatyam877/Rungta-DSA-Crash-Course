b8b4028cdf48450983933832281d05bceb78e930
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-08-20 10:20:52 +0530

    Chocolate Game

--- src/Lec1/Lec.java
+++ src/Lec1/Lec.java
@@ -70,6 +70,111 @@ public class Lec {
 
         // https://www.codechef.com/problems/CHOCGM
 
+        /*
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+
+       static class FastReader {
+
+        // BufferedReader to read input
+        BufferedReader b;
+
+        // StringTokenizer to tokenize input
+        StringTokenizer s;
+
+        // Constructor to initialize BufferedReader
+        public FastReader() {
+            b = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        // Method to read the next token as a string
+        String next() {
+            while (s == null || !s.hasMoreElements()) {
+                try {
+                    s = new StringTokenizer(b.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return s.nextToken();
+        }
+
+        // Method to read the next token as an integer
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        // Method to read the next token as a long
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        // Method to read the next token as a double
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        // Method to read the next line as a string
+        String nextLine() {
+            String str = "";
+            try {
+                if (s.hasMoreTokens()) {
+                    str = s.nextToken("\n");
+                } else {
+                    str = b.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void solve(FastReader s) {
+        int n = s.nextInt();
+
+        int [] a = new int[n + 1];
+        int T = 0 , O = 0;
+
+        for(int i = 1; i <= n; ++i) {
+            a[i] = s.nextInt();
+            T += a[i];
+
+            if(a[i] % 2 != 0)
+                ++O;
+        }
+
+        if(T % 2 == 0) {
+            int A = T - O;
+            A += (O / 2);
+            System.out.println(A);
+        } else {
+            int B = T - O;
+            B += ((O / 2) + 1);
+
+            System.out.println(T - B);
+        }
+    }
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        FastReader s = new FastReader();
+        int t = s.nextInt();
+
+        while(t-- > 0)
+            solve(s);
+	}
+}
+
+        */
+
+        // https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
+
         FastReader s = new FastReader();
         int t = s.nextInt();
 
