bb9b7724753bf44db9175579f6fcff696c6d4586
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-08-21 10:34:16 +0530

    Collections to Binary Search

--- src/Lec2/Lec2.java
+++ src/Lec2/Lec2.java
@@ -1,12 +1,78 @@
 package Lec2;
 
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Lec2 {
     public static void main(String [] args) {
         // https://leetcode.com/problems/top-k-frequent-elements/description/
+        /*
+class Solution {
+    class Pair implements Comparable<Pair>{
+        Integer first , second;
+
+        Pair(Integer first , Integer second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public int compareTo(Pair right) {
+            if(this.first.compareTo(right.first) == 0)
+                return this.second.compareTo(right.second);
+
+            return this.first.compareTo(right.first);
+        }
+    }
+
+    public int[] topKFrequent(int[] A, int K) {
+        HashMap<Integer , Integer> mp = new HashMap<>();
+
+        int N = A.length;
+
+        for(int i = 0; i < N; ++i)
+            if(mp.get(A[i]) != null)
+                mp.put(A[i] , mp.get(A[i]) + 1);
+            else
+                mp.put(A[i] , 1);
+
+        TreeSet<Pair> ts = new TreeSet<>();
+
+        for(Integer Key : mp.keySet()) {
+            Pair C = new Pair(Key , mp.get(Key));
+            if(ts.size() < K)
+                ts.add(new Pair(C.second , C.first));
+            else {
+                Pair L = ts.getFirst();
+
+                if(C.second > L.first) {
+                    ts.remove(L);
+                    ts.add(new Pair(C.second , C.first));
+                }
+            }
+        }
+
+        int [] ans = new int [K];
+        int inx = 0;
+
+        for(Pair C : ts)
+             ans[inx++] = C.second;
+
+        return ans;
+    }
+}
+
+        */
+
+
         // https://leetcode.com/problems/roman-to-integer/description/?topicSlugs=array
         // https://leetcode.com/problems/k-closest-points-to-origin/
 
         /*     Binary Search     */
+
+
+//        TreeSet<Integer> ts = new TreeSet<>();
 
         // https://leetcode.com/problems/first-bad-version/description/
         // https://leetcode.com/problems/sqrtx/
