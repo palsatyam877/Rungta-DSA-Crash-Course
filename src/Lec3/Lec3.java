b0e8f12185a1efe09b9ed74eec4b78edaf901374
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-08-22 10:32:01 +0530

    1482. Minimum Number of Days to Make m Bouquets

--- src/Lec3/Lec3.java
+++ src/Lec3/Lec3.java
@@ -7,6 +7,101 @@ public class Lec3 {
 }
 
 // https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+
+/*
+class Solution {
+    public boolean Check(int [] B , int m , int k , int D) {
+        int N = B.length;
+        int C = 0 , Boq = 0;
+
+        for(int i = 0; i < N; ++i)
+            if(B[i] <= D) {
+               ++C;
+
+                if(C == k) {
+                   Boq++;
+                   C = 0;
+                }
+            } else
+                C = 0;
+
+        return Boq >= m;
+    }
+
+    public int minDays(int[] B, int m, int k) {
+        long ub = 0;
+
+        for(int i = 0; i < B.length; ++i)
+            ub = Math.max(ub , B[i]);
+
+        int ans = -1;
+
+        for(int D = 1; D <= ub; ++D) {
+            boolean A = Check(B , m , k , D);
+
+            if(A) {
+                if(ans == -1)
+                   ans = D;
+                 System.out.print("T");
+            } else
+                System.out.print("F");
+        }
+
+        return ans;
+    }
+}
+
+*/
+
+/*
+class Solution {
+    public boolean Check(int [] B , int m , int k , long D) {
+        int N = B.length;
+        int C = 0 , Boq = 0;
+
+        for(int i = 0; i < N; ++i)
+            if(B[i] <= D) {
+               ++C;
+
+                if(C == k) {
+                   Boq++;
+                   C = 0;
+                }
+            } else
+                C = 0;
+
+        return Boq >= m;
+    }
+
+    public int minDays(int[] B, int m, int k) {
+        long ub = 0;
+
+        for(int i = 0; i < B.length; ++i)
+            ub = Math.max(ub , B[i]);
+
+        long ans = -1;
+
+        long lo = 1 , hi = ub;
+
+        for(;lo <= hi;) {
+            long D = (lo + hi) / 2;
+
+            boolean A = Check(B , m , k , D);
+
+            if(A) {
+                ans = D;
+                hi = D - 1;
+            } else
+                lo = D + 1;
+
+        }
+
+        return (int)ans;
+    }
+}
+
+*/
+
 // https://codeforces.com/contest/1873/problem/E
 
 /**** Hard Problem ****/
