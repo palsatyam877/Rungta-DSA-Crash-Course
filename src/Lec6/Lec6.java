a41b1c383417a5f862da436fa0f3cbe5db9752fe
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-08-26 11:54:46 +0530

    Forest Queries

--- src/Lec6/Lec6.java
+++ src/Lec6/Lec6.java
@@ -2,7 +2,15 @@ package Lec6;
 
 public class Lec6 {
     public static void main(String [] args) {
-        System.out.println("Jai Maa Bhavani");
+//        System.out.println("Jai Maa Bhavani");
+
+        Character x = 'D';
+
+        int a = (int)x - (int)('A');
+
+        System.out.println(a);
+
+//        System.out.println(p);
     }
 }
 
@@ -54,7 +62,112 @@ class Solution {
 */
 
 // https://leetcode.com/problems/minimum-window-substring/
-// https://leetcode.com/problems/sum-of-subarray-minimums/
+
+/*
+class Solution {
+    public String minWindow(String S, String T) {
+        int N = S.length() , M = T.length();
+
+        int [][] Ps = new int [N + 1][60];
+        int [] Ts = new int [60];
+
+        for(int i = 0; i < N; ++i) {
+            Ps[i + 1][(int)S.charAt(i) - (int)('A')]++;
+
+                for(int j = 0; j < 60; ++j)
+                   Ps[i + 1][j] += Ps[i][j];
+        }
+
+        for(int i = 0; i < M; ++i)
+            Ts[(int)T.charAt(i) - (int)('A')]++;
+
+        int L = -1 , R = -1;
+
+        for(int r = 0; r < N; ++r) {
+            int lo = 1 , hi = r + 1;
+            int TmpL = -1;
+
+            while(lo <= hi) {
+                int mid = (lo + hi) / 2;
+                boolean isCap = true;
+
+                for(int j = 0; j < 60; ++j)
+                    isCap &= (Ps[r + 1][j] - Ps[mid - 1][j] >= Ts[j]);
+
+                if(!isCap)
+                    hi = mid - 1;
+                else {
+                    TmpL = mid;
+                    lo = mid + 1;
+                }
+            }
+
+            if(TmpL != -1) {
+                if(L == -1) {
+                    L = TmpL;
+                    R = r + 1;
+                } else {
+                    if(R - L + 1 > r - TmpL + 1) {
+                      L = TmpL;
+                      R = r + 1;
+                    }
+                }
+            }
+        }
+
+        StringBuffer ans = new StringBuffer();
+
+        if(L == -1)
+           return ans.toString();
+        else {
+            for(int i = L; i <= R; ++i)
+                ans.append(S.charAt(i - 1));
+
+            return ans.toString();
+        }
+    }
+}
+*/
+
 // https://cses.fi/problemset/task/1652
+/*
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main() {
+	int n , q;
+	cin >> n >> q;
+
+	vector<vector<char>> M;
+
+	M.assign(n + 1 , vector<char>(n + 1 , '.'));
+
+	for(int i = 1; i <= n; ++i)
+	  for(int j = 1; j <= n; ++j)
+	     cin >> M[i][j];
+
+    vector<vector<int>> P(n + 1 , vector<int>(n + 1 , 0));
+
+    for(int i = 1; i <= n; ++i)
+       for(int j = 1; j <= n; ++j) {
+          P[i][j] = P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1];
+
+          if(M[i][j] == '*')
+          	  P[i][j]++;
+       }
+
+
+     for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j)
+           cout << P[i][j] << " \n"[j == n];
+
+}
+
+*/
+
 // https://leetcode.com/problems/subarray-product-less-than-k/submissions/2117041357/
 // https://leetcode.com/problems/sliding-window-median/
+
+// https://leetcode.com/problems/sum-of-subarray-minimums/
