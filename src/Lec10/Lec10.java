b15c4f8e1c8fec25c5190408def9df0ddc8c984d
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-09-01 09:59:10 +0530

    199. Binary Tree Right Side View

--- src/Lec10/Lec10.java
+++ src/Lec10/Lec10.java
@@ -7,6 +7,53 @@ public class Lec10 {
 }
 
 // https://leetcode.com/problems/binary-tree-right-side-view/
+/*
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayDeque<TreeNode> qu = new ArrayDeque<>();
+        int N = -100001;
+
+        if(root == null) {
+           return new ArrayList<>();
+        }
+
+        TreeNode NULL = new TreeNode(N);
+        qu.add(NULL);
+        qu.add(root);
+
+        int prev = N;
+
+        List<Integer> ans = new ArrayList<>();
+
+        while(qu.size() > 0) {
+            TreeNode C = qu.poll();
+
+            if(C.val == N) {
+                if(prev != N)
+                    ans.add(prev);
+
+                if(qu.size() != 0)
+                    qu.offer(NULL);
+                else
+                   break;
+
+                continue;
+            } else
+                prev = C.val;
+
+
+            if(C.left != null)
+                qu.offer(C.left);
+
+            if(C.right != null)
+                qu.offer(C.right);
+        }
+
+        return ans;
+    }
+}
+*/
+
 // https://leetcode.com/problems/count-good-nodes-in-binary-tree/
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
 // https://cses.fi/problemset/task/1674
