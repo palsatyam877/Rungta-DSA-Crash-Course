0cb1e156309a92aa69ad404608ea6578dd56dba2
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-09-01 09:29:31 +0530

    Trees Continue

---
+++ src/Lec10/Lec10.java
@@ -0,0 +1,15 @@
+package Lec10;
+
+public class Lec10 {
+    public static void main(String [] args) {
+        System.out.println("Jai Maa Bhavani");
+    }
+}
+
+// https://leetcode.com/problems/binary-tree-right-side-view/
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// https://cses.fi/problemset/task/1674
+
+// https://leetcode.com/problems/validate-binary-search-tree/
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
--- src/Lec9/Lec9.java
+++ src/Lec9/Lec9.java
@@ -379,11 +379,3 @@ class Solution {
 
 */
 
-// https://leetcode.com/problems/binary-tree-right-side-view/
-// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
-// https://leetcode.com/problems/binary-tree-maximum-path-sum/
-// https://cses.fi/problemset/task/1674
-
-// https://leetcode.com/problems/validate-binary-search-tree/
-// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-
