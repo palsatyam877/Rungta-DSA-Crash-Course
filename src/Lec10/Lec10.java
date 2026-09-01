e65005ac9832c549a8d488e044f3a3c55868ebda
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-09-01 16:29:17 +0530

    98. Validate Binary Search Tree

--- src/Lec10/Lec10.java
+++ src/Lec10/Lec10.java
@@ -2,15 +2,87 @@ package Lec10;
 import com.sun.source.tree.ArrayAccessTree;
 
 import java.util.*;
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Lec10 {
+    static class FastReader {
+
+        // BufferedReader to read input
+        BufferedReader b;
+
+        // StringTokenizer to tokenize input
+        StringTokenizer s;
+
+        // Constructor to initialize BufferedReader
+        public FastReader() {
+            b = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        // Method to read the next token as a string
+        String next() {
+            while (s == null || !s.hasMoreElements()) {
+                try {
+                    s = new StringTokenizer(b.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return s.nextToken();
+        }
+
+        // Method to read the next token as an integer
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        // Method to read the next token as a long
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        // Method to read the next token as a double
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        // Method to read the next line as a string
+        String nextLine() {
+            String str = "";
+            try {
+                if (s.hasMoreTokens()) {
+                    str = s.nextToken("\n");
+                } else {
+                    str = b.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+    public static int dfs(int curr , ArrayList<ArrayList<Integer>> adj , int [] A) {
+          int tot = 1;
+
+          for(int child : adj.get(curr)) {
+             tot +=  dfs(child , adj , A);
+          }
+
+          A[curr] = tot - 1;
+
+          return tot;
+    }
+
     public static void main(String [] args) {
-        System.out.println("Jai Maa Bhavani");
+//        System.out.println("Jai Maa Bhavani");
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
 
         int n = sc.nextInt();
         int [] subord = new int [n + 1];
@@ -22,12 +94,14 @@ public class Lec10 {
             adj.add(new ArrayList<>());
 
         for(int i = 2; i <= n; ++i)
-            adj.get(i).add(subord[i]);
+            adj.get(subord[i]).add(i);
 
-        for(int i = 1; i <= n; ++i) {
-            System.out.print(i + " -> ");
-            System.out.println(subord[i]);
-        }
+        int [] A = new int[n + 1];
+
+        dfs(1 , adj , A);
+
+        for(int i = 1; i <= n; ++i)
+            System.out.print(A[i] + " ");
     }
 }
 
@@ -127,7 +201,6 @@ class Solution {
         return dfs(root , -100000);
     }
 }
-
 */
 
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/
@@ -167,6 +240,163 @@ class Solution {
 */
 
 // https://cses.fi/problemset/task/1674
+/*
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Lec10 {
+    static class FastReader {
+
+        // BufferedReader to read input
+        BufferedReader b;
+
+        // StringTokenizer to tokenize input
+        StringTokenizer s;
+
+        // Constructor to initialize BufferedReader
+        public FastReader() {
+            b = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        // Method to read the next token as a string
+        String next() {
+            while (s == null || !s.hasMoreElements()) {
+                try {
+                    s = new StringTokenizer(b.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return s.nextToken();
+        }
+
+        // Method to read the next token as an integer
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        // Method to read the next token as a long
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        // Method to read the next token as a double
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        // Method to read the next line as a string
+        String nextLine() {
+            String str = "";
+            try {
+                if (s.hasMoreTokens()) {
+                    str = s.nextToken("\n");
+                } else {
+                    str = b.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+
+    public static int dfs(int curr , ArrayList<ArrayList<Integer>> adj , int [] A) {
+          int tot = 1;
+
+          for(int child : adj.get(curr)) {
+             tot +=  dfs(child , adj , A);
+          }
+
+          A[curr] = tot - 1;
+
+          return tot;
+    }
+
+    public static void main(String [] args) {
+//        System.out.println("Jai Maa Bhavani");
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
+
+        int n = sc.nextInt();
+        int [] subord = new int [n + 1];
+
+        for(int i = 2; i <= n; ++i)
+            subord[i] = sc.nextInt();
+
+        for(int i = 0; i <= n; ++i)
+            adj.add(new ArrayList<>());
+
+        for(int i = 2; i <= n; ++i)
+            adj.get(subord[i]).add(i);
+
+        int [] A = new int[n + 1];
+
+        dfs(1 , adj , A);
+
+        for(int i = 1; i <= n; ++i)
+            System.out.print(A[i] + " ");
+    }
+}
+*/
 
 // https://leetcode.com/problems/validate-binary-search-tree/
+/*
+class Solution {
+    public class Pair {
+        Long mn , mx;
+        Boolean bst;
+
+        Pair(Long mn , Long mx , Boolean bst) {
+            this.mn = mn;
+            this.mx = mx;
+            this.bst = bst;
+        }
+    }
+
+    public Pair dfs(TreeNode Curr) {
+        long MX = (long)1e18;
+
+        Pair L = new Pair(MX , MX , true);
+        Pair R = new Pair(-MX , -MX , true);
+
+        if(Curr.left != null)
+           L = dfs(Curr.left);
+
+        if(Curr.right != null)
+           R = dfs(Curr.right);
+
+        long CurrMn = MX , CurrMx = -MX;
+        boolean CurrBst = true;
+
+        if(L.mx == MX)
+            CurrMn = Curr.val;
+        else {
+            CurrMn = L.mn;
+            CurrBst &= (Curr.val > L.mx) && L.bst;
+        }
+
+        if(R.mx == -MX)
+            CurrMx = Curr.val;
+        else {
+            CurrMx = R.mx;
+            CurrBst &= (Curr.val < R.mn) && R.bst;
+        }
+
+        return new Pair( CurrMn , CurrMx , CurrBst );
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root).bst;
+    }
+}
+*/
+
+
 // https://leetcode.com/problems/kth-smallest-element-in-a-bst/
