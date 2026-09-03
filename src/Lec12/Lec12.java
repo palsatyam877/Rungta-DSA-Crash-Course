89dd9ad331754745820ee3172870b57c8a28be6d
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-09-03 10:07:25 +0530

    Modular Exponentiation Solution

--- src/Lec12/Lec12.java
+++ src/Lec12/Lec12.java
@@ -7,3 +7,29 @@ public class Lec12 {
 }
 
 // https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
+/*
+class Solution {
+    long f(long X , long N , long M) {
+        if(N == 1)
+            return X;
+
+        if(N == 2)
+           return (X * X) % M;
+
+        if((N & 1) != 0) {
+            long C = f(X , N / 2 , M);
+
+            return (X * ((C * C)%M))%M;
+        } else {
+            long C = f(X , N / 2 , M);
+
+            return (C * C) % M;
+        }
+    }
+
+    public int powMod(int x, int n, int M) {
+        // code here
+        return (int)f((long)x , (long)n , (long)M);
+    }
+}
+*/
