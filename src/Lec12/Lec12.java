e85a3f1e6effe7c14a8f730a7dd7ffa90cf29b60
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2026-09-03 09:42:02 +0530

    Modular Exponentiation

---
+++ src/Lec12/Lec12.java
@@ -0,0 +1,9 @@
+package Lec12;
+
+public class Lec12 {
+    public static void main(String [] args) {
+        System.out.println("Jai Maa Jagad Amba");
+    }
+}
+
+// https://www.geeksforgeeks.org/problems/modular-exponentiation-for-large-numbers5537/1
